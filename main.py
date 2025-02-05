af6f7a9335717c32b77b70e3eead44457d129ba2
Matt VerLee <mverlee7@gmail.com>
2025-02-05 12:17:57 -0700

    Add typing when bot is pinged

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ async def on_message_create(event: interactions.api.events.MessageCreate):
         if bot_mention not in message.content and not message.attachments and not is_reply_to_bot:
             return
         elif bot_mention in message.content:
-            bot.trigger_typing(message.channel)
+            message.channel.trigger_typing()
 
         # Log the user prompt (replacing the raw bot mention with a friendly name).
         message_formatted = message.content.replace(bot_mention, "@ChatGPT")
