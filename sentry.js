93b71b6ab3f3fa2068240e0ff5d2c18e34da8ba9
doubleangels <matt@doubleangels.com>
2025-03-28 12:43:30 -0600

    Remove performance monitoring notice

--- sentry.js
+++ sentry.js
@@ -1,16 +1,13 @@
 const Sentry = require("@sentry/node");
-const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 const path = require('path');
 const logger = require('./logger')(path.basename(__filename));
 
 // Initialize Sentry for error tracking and performance monitoring.
 Sentry.init({
   dsn: "https://eec36346892467255ce18e6fed4ef80d@o244019.ingest.us.sentry.io/4508717394034688",
-  integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0, // Capture all trace data.
-  profilesSampleRate: 1.0, // Capture all profiling data.
 });
-logger.info("Sentry initialized with performance monitoring.");
+logger.info("Sentry initialized!");
 
 // Export the configured Sentry instance
 module.exports = Sentry;
