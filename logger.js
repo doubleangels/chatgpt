931cd7db14c41de746caf5e81ca4a98fdffb7223
Matt VerLee <matt@doubleangels.com>
2025-06-11 15:22:14 -0600

    Fix bug with output logging

--- logger.js
+++ logger.js
@@ -1,4 +1,5 @@
 const { createLogger, format, transports } = require('winston');
+const util = require('util');
 const config = require('./config');
 
 /** Log format field names */
@@ -27,18 +28,23 @@ function getLogger(label) {
       format.label({ label }),
       format.timestamp(),
       format.printf(({ timestamp, level, message, label, ...meta }) => {
-        // Handle format specifiers in the message
+        // Format the message with any additional arguments
         let formattedMessage = message;
         if (typeof message === 'string' && meta && meta[0] !== undefined) {
           const args = Array.isArray(meta[0]) ? meta[0] : [meta[0]];
-          formattedMessage = message.replace(/%[sd]/g, () => args.shift() || '');
+          formattedMessage = util.format(message, ...args);
+        }
+
+        // Remove the first meta entry if it was used for formatting
+        if (meta && meta[0] !== undefined) {
+          delete meta[0];
         }
 
         return LOG_FORMAT_TEMPLATE.replace('%s', timestamp)
           .replace('%s', label)
           .replace('%s', level.toUpperCase())
           .replace('%s', formattedMessage)
-          .replace('%s', Object.keys(meta).length > 1 ? JSON.stringify(meta) : '');
+          .replace('%s', Object.keys(meta).length ? JSON.stringify(meta) : '');
       })
     ),
     transports: [new transports.Console()]
